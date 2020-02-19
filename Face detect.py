4b5ea20043fbf34d97003cea00d393a302b3bd51
Malhaar Arora <58373047+malhaar2002@users.noreply.github.com>
2020-02-19 19:41:19 +0530

    Update Face detect.py

--- Face detect.py
+++ Face detect.py
@@ -1,7 +1,7 @@
 import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img = cv2.imread("IMG_20181029_163646.jpg")
+img = cv2.imread("image link")
 
 cv2.imshow("original", img)
 cv2.waitKey(0)
@@ -9,7 +9,7 @@ cv2.destroyAllWindows()
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.1, minNeighbors = 5)
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05, minNeighbors = 5)
 
 for x, y, w, h in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 3)
